bb74fc09ec223f51a7fe23ae2e68be588f42d155
WeslySloan <88778903+WeslySloan@users.noreply.github.com>
2025-04-10 19:51:23 +0900

    (2025.04.10) TopDown 셋업

---
+++ TopDownTutorial/Source/TopDownTutorial/TopDownTutorial.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TopDownTutorial.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TopDownTutorial, "TopDownTutorial" );
